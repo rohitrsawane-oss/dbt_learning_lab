79d393fd18936fe7873bb7464c3be8e89c5c98f4
Rohit Sawane <rohit.r.sawane@kipi.ai>
2026-06-23 17:01:58 +0000

    created staging layer

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,17 @@
+select 
+o.orderid,
+o.orderdate,
+o.shipdate,
+o.shipmode,
+c.customerid,
+c.customername,
+c.segment,
+p.productid,
+p.category,
+p.subcategory,
+(o.ordersellingprice - o.ordercostprice) as orderprofit
+from {{ ref('raw_orders') }} as o
+left join {{ ref('raw_customers') }} as c
+on c.customerid = o.customerid
+left join {{ ref('raw_products') }} as p 
+on p.productid = o.productid
